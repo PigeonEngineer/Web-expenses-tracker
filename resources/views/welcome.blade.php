feea889abe0834828e9e3cb43a67c0514cda6345
MahStuff <georgs12@gmail.com>
2018-05-03 00:15:35 +0300

    Navbar in the homepage draft

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,10 +84,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://localhost:8000/newHome">Our home page draft</a>
                 </div>
             </div>
         </div>
